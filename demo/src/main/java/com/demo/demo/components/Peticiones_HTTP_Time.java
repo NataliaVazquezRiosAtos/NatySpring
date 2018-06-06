42fc4d706160c788241ce9af5f61970141265800
A697622 <A697622@MC0YUFYC.ww930.my-it-solutions.net>
2018-06-06 09:51:33 +0200

    cambios para el video 33. creo clase para calculo de tiempo de las peticiones http. creo clase de configuracion mvc web

---
+++ demo/src/main/java/com/demo/demo/components/Peticiones_HTTP_Time.java
@@ -0,0 +1,56 @@
+/*
+ * esta clase va a hacer que por cada peticion http que nos hagan entre por esta clase a traves de los siguiemtes metodos
+ * sobreescritos ( heredados de la clase HandlerInterceptorAdapter ) 
+ * 
+ * para calcular el tiempo y mostrarlo en el log : 
+ * 
+ * */
+package com.demo.demo.components;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component("requestTimeInerceptor")
+public class Peticiones_HTTP_Time extends HandlerInterceptorAdapter{
+	
+	public static final Log MI_LOG = LogFactory.getLog(Peticiones_HTTP_Time.class);
+	
+	// PRIMERO
+	// es el que se ejecuta justo antes de presentar la vista en el navegador ( antes del return )
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+				
+		request.setAttribute("startTime", System.currentTimeMillis());
+		
+		return true;
+		
+	}
+	
+	
+	// SEGUNDO
+	// es el metodo que se ejecuta antes de entrar en el metodo del controlador
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {	
+		
+		long startTiempo = (long) request.getAttribute("startTime"); 
+	
+		MI_LOG.info(" -- Respuesta URL: "+ request.getRequestURL().toString() + " -- TIEMPO TOTAL: " +( System.currentTimeMillis() - startTiempo)+"´ms");
+		
+	}
+	
+		
+	
+	
+
+
+
+
+
+	
+	
+}
---
+++ demo/src/main/java/com/demo/demo/configuration/ConfigurationWebMvc.java
@@ -0,0 +1,28 @@
+package com.demo.demo.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.demo.demo.components.Peticiones_HTTP_Time;
+
+@Configuration
+public class ConfigurationWebMvc extends WebMvcConfigurerAdapter {
+	
+	// le decimos a spring que se encuentra en su memoria
+	@Autowired
+	@Qualifier("requestTimeInerceptor")
+	
+	private Peticiones_HTTP_Time requestTime;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+
+		registry.addInterceptor(requestTime);
+		
+	}
+
+	
+}
