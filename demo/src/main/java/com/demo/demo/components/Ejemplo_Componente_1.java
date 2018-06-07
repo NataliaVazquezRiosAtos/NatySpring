5658e526adaec3b2e620eac775e1f20d948285eb
A697622 <A697622@MC0YUFYC.ww930.my-it-solutions.net>
2018-06-07 15:25:42 +0200

    combios para crear mi primer JpaRepository

--- demo/src/main/java/com/demo/demo/components/Ejemplo_Componente_1.java
+++ demo/src/main/java/com/demo/demo/components/Ejemplo_Componente_1.java
@@ -2,19 +2,25 @@ package com.demo.demo.components;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.demo.demo.controller.ControllerFormulario;
+import com.demo.demo.repository.CursoJpaRepository;
 
 @Component("componente_de_ejemplo")
 public class Ejemplo_Componente_1 {
 	
 	private final Log MI_LOG = LogFactory.getLog(Ejemplo_Componente_1.class);
 	
+	@Autowired
+	@Qualifier("cursoJpaRepository")
+	private CursoJpaRepository jpaRepositoryCurso;
+	
 	public void ver() {
 		
-		MI_LOG.info("hola desde Ejemplo_Componente_1");
-		
+		MI_LOG.info("hola desde Ejemplo_Componente_1");		
 		
 	}
 
---
+++ demo/src/main/java/com/demo/demo/repository/CursoJpaRepository.java
@@ -0,0 +1,71 @@
+package com.demo.demo.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.demo.demo.entity.Curso;
+
+/*
+ * esta interface nos permite también, poder crear metodos personalizados que sigan una logica que JpaRepository
+ * acepte , el cual no nos haga falta ningun tipo de implementacion ( es una interfaz )
+ * 
+ * */
+
+@Repository("cursoJpaRepository")
+public interface CursoJpaRepository extends JpaRepository<Curso, Serializable> {
+	
+	// solo con las dos lineas anteriores esto ya tendriamos un repositorio jpa
+	// JpaRepository<Curso, Serializable>    Curso --> entidad a la que va a hacer referencia
+	
+	public abstract Curso metodo();
+	
+	// JpaRepository nos permite realizar las siguientes querys : 
+	// findByNombre : hace una query por el atributo nombre del bean Curso sin implementar nada
+	// se podria hacer para cada uno de los atributos del bean : findByDescripcion ,findByPrecio , etc...
+	
+	public abstract Curso findByNombre(String nombre);
+	
+	// tambien se pueden hacer consultas compueastas :
+	// hace una query por los dos atributos sin necesidad e implementar nada
+	
+	public abstract Curso findByNombreAndDescripcion( String nombre , String descripcion );
+	
+	// hace una query por nombre ordenandolos por numero de horas , sin necesidad e implementar nada
+	
+	public abstract List<Curso> findByNombreOrderByHoras (String nombre);
+	
+	// hace una query por los dos atributos sin necesidad e implementar nada
+	
+	public abstract List<Curso> findByNombreOrPrecio (String nombre, int precio);
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
