ffa929e5e99bb425d0c5f199fa37e6ed5375369e
A697622 <A697622@MC0YUFYC.ww930.my-it-solutions.net>
2018-06-09 15:25:45 +0200

    commit de inicio

---
+++ NatySpring/src/main/java/com/natySpring/NatySpringApplication.java
@@ -0,0 +1,12 @@
+package com.natySpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatySpringApplication.class, args);
+	}
+}
---
+++ NatySpring/src/main/java/com/natySpring/configuration/ConfigurationWebMvc_Mod.java
@@ -0,0 +1,29 @@
+package com.natySpring.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.natySpring.components.Peticiones_HTTP_Time;
+
+@Configuration
+public class ConfigurationWebMvc_Mod extends WebMvcConfigurerAdapter {
+	
+	// le decimos a spring que se encuentra en su memoria
+	@Autowired
+	@Qualifier("requestTimeInerceptor")
+	
+	private Peticiones_HTTP_Time requestTime;
+
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+
+		registry.addInterceptor(requestTime);
+		
+	}
+
+	
+}
